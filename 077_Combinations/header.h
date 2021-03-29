7e28ce9166395f5d9ed451ecbef28620ee53c0a8
Michael <kindsword@live.com>
2021-03-29 14:43:10 -0400

    Fixing a name.

--- 077_Combinations/header.h
+++ 077_Combinations/header.h
@@ -2,6 +2,6 @@
 
 #include "../common/common.h"
 
-void PrepareTestData(vector<TD_I_I_VVI>& test_data);
+void PrepareTestData(vector<TD_I_I_VVI>& testData);
 
 vector<vector<int>> combine(int n, int k);
